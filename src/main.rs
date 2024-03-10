1d5b18beff6b19058b79327058d0c554a1f719ef
OHMCFXG <jianghaobing3690@gmail.com>
2024-03-10 19:22:18 +0800

    support set relative value

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::fs::read_dir;
+use std::{cmp, fs};
+use std::fs::{File, read_dir};
+use std::io::Write;
 use std::num::ParseIntError;
 
 use clap::Parser;
 use ddc::Ddc;
-use eyre::{Result, eyre};
+use eyre::{eyre, Result};
 
 fn parse_feature_code(input: &str) -> Result<u8, ParseIntError> {
   if let Some(s) = input.strip_prefix("0x") {
@@ -24,56 +26,84 @@ struct Cli {
   #[clap(value_parser = parse_feature_code)]
   feature_code: u8,
   /// value to be set; when not present show current value
-  feature_value: Option<u16>,
+  feature_value: Option<String>,
 }
 
 // /sys/class/drm/card*-{name}/i2c-*
 fn get_i2c_dev(output: &str) -> Result<String> {
-  let mut output_dir = None;
-  for entry in read_dir("/sys/class/drm").unwrap() {
+  let mut dev = None;
+  for entry in read_dir("/sys/class/i2c-dev").unwrap() {
     let path = entry.unwrap().path();
+    let link_path = path.read_link().unwrap();
     let name = path.file_name().unwrap().to_str().unwrap();
-    if name.starts_with("card") && name.ends_with(output) {
-      let before_name = name.len() - output.len() - 1;
-      if &name[before_name..before_name+1] == "-" {
-        output_dir = Some(path);
+    let points_to = link_path.to_str().unwrap();
+    let points_to_split = points_to.split('/').collect::<Vec<_>>();
+    for item in points_to_split {
+      if item.starts_with("card") && item.ends_with(output) {
+        dev = Some(name.to_string());
         break;
       }
     }
-  };
-  let mut dev = None;
-  let output_dir = output_dir.ok_or_else(|| eyre!("output name not found in /sys/class/drm"))?;
-  for entry in read_dir(output_dir).unwrap() {
-    let entry = entry.unwrap();
-    let file_name = entry.file_name();
-    let name = file_name.to_str().unwrap();
-    if name.starts_with("i2c-") {
-      dev = Some(name.to_owned());
-    } else if name == "ddc" {
-      let link = entry.path().read_link().unwrap();
-      dev = Some(link.file_name().unwrap().to_string_lossy().into_owned())
+    if dev.is_some() {
+      break;
     }
-  }
+  };
 
   dev.ok_or_else(|| eyre!("i2c dev not found"))
 }
 
+fn set_value(ddc: &mut ddc_i2c::I2cDeviceDdc, feature_code: u8, value: String) -> Result<()> {
+  let current = ddc.get_vcp_feature(feature_code)?;
+  let current_value = current.value();
+  let max = current.maximum();
+  let mut new_value = current_value;
+  if value.ends_with('+') || value.ends_with('-') {
+    let relative = value.trim_end_matches(|c| c == '+' || c == '-');
+    let relative = relative.parse::<u16>()?;
+    // make sure we don't go below 0
+    if value.ends_with("-") {
+      new_value = current_value.saturating_sub(relative);
+    }
+    // make sure we don't go above max
+    if value.ends_with("+") {
+      new_value = cmp::min(max, current_value + relative)
+    }
+  } else {
+    new_value = value.parse::<u16>()?;
+    new_value = if new_value > max { max } else { new_value };
+  }
+  ddc.set_vcp_feature(feature_code, new_value)?;
+
+  Ok(())
+}
+
 fn main() -> Result<()> {
   let cli = Cli::parse();
-  let i2c_name = if cli.output_name.starts_with("i2c-") {
-    cli.output_name
+  let output_name = cli.output_name;
+  let i2c_name = if output_name.starts_with("i2c-") {
+    output_name.clone()
   } else {
-    get_i2c_dev(&cli.output_name)?
+    get_i2c_dev(&output_name)?
   };
   let dev = format!("/dev/{}", i2c_name);
   let mut ddc = ddc_i2c::from_i2c_device(dev).unwrap();
+
   if let Some(v) = cli.feature_value {
-    ddc.set_vcp_feature(cli.feature_code, v)?;
-    println!("{}", v);
+    set_value(&mut ddc, cli.feature_code, v)?;
   } else {
-    let value = ddc.get_vcp_feature(cli.feature_code)?;
-    println!("{} {}", value.value(), value.maximum());
+    ddc.get_vcp_feature(cli.feature_code)?;
   }
+
+  let current = ddc.get_vcp_feature(cli.feature_code)?;
+  let output_json = format!("{{\"value\":{}, \"percentage\":{:.0}, \"max\":{}}}",
+                            current.value(),
+                            (current.value() as f32 / current.maximum() as f32) * 100.0,
+                            current.maximum());
+  fs::create_dir_all("/tmp/backlight")?;
+  let mut file = File::create(format!("/tmp/backlight/{}.json", output_name))?;
+  write!(file, "{}", output_json)?;
+
+  println!("{}", output_json);
 
   Ok(())
 }
